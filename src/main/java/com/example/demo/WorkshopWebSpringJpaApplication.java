26dc86346486a037317b29d4ebdcd73d472ed656
victorcvs <victorcvs@gmail.com>
2020-06-05 17:40:49 -0300

    Project created

---
+++ src/main/java/com/example/demo/WorkshopWebSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWebSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopWebSpringJpaApplication.class, args);
+	}
+
+}
